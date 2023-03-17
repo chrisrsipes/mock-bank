c34574ce10393b024da6b88cc1dee11d4801b242
Christopher Sipes <chrisrsipes@gmail.com>
2023-03-17 00:46:30 -0400

    implementation for JWT support - refactored some configs out of SecurityConfig into new ApplicationConfig - remove default user details service with custom one to look at our user entity table - jwt service for jwt utility methods such as generating jwt and decoding jwt - authentication filter and mixing it into security chain - authentication controller and service

---
+++ src/main/java/crs/projects/mockbank/config/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package crs.projects.mockbank.config;
+
+import crs.projects.mockbank.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .csrf().disable()
+                .cors().and()
+                .authorizeHttpRequests()
+                .requestMatchers("/auth/**").permitAll()
+                .requestMatchers("/public").permitAll() // permitting these are not currently working
+                .requestMatchers("/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+}
---
+++ src/main/java/crs/projects/mockbank/service/AuthenticationService.java
@@ -0,0 +1,60 @@
+package crs.projects.mockbank.service;
+
+
+import crs.projects.mockbank.config.JwtService;
+import crs.projects.mockbank.dto.AuthenticationRequest;
+import crs.projects.mockbank.dto.AuthenticationResponse;
+import crs.projects.mockbank.dto.RegisterRequest;
+import crs.projects.mockbank.model.Role;
+import crs.projects.mockbank.model.User;
+import crs.projects.mockbank.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        User newUser = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        User savedUser = userRepository.save(newUser);
+        String token = jwtService.generateToken(savedUser);
+
+        return AuthenticationResponse.builder()
+                .token(token)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        // has side effect of throwing exception if not successful
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+        );
+
+        User user = userRepository.findUserByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(token)
+                .build();
+    }
+}
